b0af804e4c26e40c332b5d84795ab466876ee743
RSNF <ray1001novaes@gmail.com>
2024-04-10 15:02:10 -0300

    feat: upgrade the UI, add stylization and JS event

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const menuSection = document.querySelector(".menu");
+const recipeSection = document.querySelector(".recipe");
+
+async function getContentFromPage(page) {
+
+    let url = `../pages/${page}.html`;
+
+    let file = await fetch(url)
+    .then((res) => res.text())
+    .then((text) => text);
+
+    recipeSection.innerHTML = file;
+
+    recipeSection.scrollIntoView(true)
+}
+
+menuSection.addEventListener("click", (e) => {
+
+    if (e.target.id != "")
+        getContentFromPage(e.target.id);
+    }
+);
